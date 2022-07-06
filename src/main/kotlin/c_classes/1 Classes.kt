8081a1b5f188b0ba3a61fffee3b21ce7f1661d6b
Enes Durak <m.enes.durak@gmail.com>
2022-07-06 14:44:25 +0300

    Classes improved

--- src/main/kotlin/c_classes/1 Classes.kt
+++ src/main/kotlin/c_classes/1 Classes.kt
@@ -1,5 +1,21 @@
 package c_classes
 
 fun main() {
+    class Car(color: String, enginePower: Float, wheelCount: Int = 4){
+        val color = color
+        private val mWheelCount: Int = 3
+        private val mDoorCount: Int = 2
 
+        init {
+            println("Initialized")
+        }
+
+        constructor(color: String, enginePower: Float, wheelCount: Int, windowColor: String): this("Red",1.6F){
+            println("Secondary constructor")
+        }
+
+    }
+
+    val car = Car("Red", 1.3F, 3,"Black")
+    println(car.color)
 }
